f6dbdea0c8dbf0d3ab256ec0b0cac2ab4a06d840
andrescaro16 <julicaro2003@gmail.com>
2023-02-25 20:09:47 -0500

    Hoisting

---
+++ src/hoisting/hoisting.js
@@ -0,0 +1,13 @@
+// var nameOfDog; //undefined
+
+// console.log(nameOfDog);
+// var nameOfDog = "Elmo";
+// console.log(nameOfDog);
+
+nameOfDog();
+
+function nameOfDog() {
+    console.log(`El mejor perrito del mundo es ${elmo}`);
+}
+
+var elmo;
